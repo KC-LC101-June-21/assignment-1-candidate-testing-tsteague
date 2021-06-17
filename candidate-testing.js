729a20e712c052d03822d9de37c3adf25fc53e9f
Telly Teague <telly.teague@gmail.com>
2021-06-17 18:55:47 -0500

    try to get green check; removing extra spaces and capitalizing per autograder

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,13 +12,13 @@ const questionsAndAnswers = (function () {
 })();
 
 let candidateName = "Participant";
-let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ',
-  '(5 + 3)/2 * 10 = ? ', 'Given the array[8, \'Orbit\', \'Trajectory\', 45], what entry is at index 2? ',
-  'What is the minimum crew size for the ISS? '];
+let questions = ['Who was the first American woman in space?', 'True or false: 5 kilometer == 5000 meters?',
+  '(5 + 3)/2 * 10 = ?', 'Given the array[8, \'Orbit\', \'Trajectory\', 45], what entry is at index 2?',
+  'What is the minimum crew size for the ISS?'];
 let correctAnswers = [ 'Sally Ride', 'true', '40', 'Trajectory', '3'];
 let candidateAnswer = "sally ride";
 let question = "Who was the first American woman in space?";
-let correctAnswer = "sally ride";
+let correctAnswer = "Sally Ride";
 let candidateAnswers = [ 'Sally Ride', 'true', '40', 'Trajectory', '3'];
 
 // TODO 1.1a: Define candidateName // 
